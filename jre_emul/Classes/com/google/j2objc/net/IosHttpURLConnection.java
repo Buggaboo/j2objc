5e82d0227eb4743c989eeeb4dd3e03573adf0d23
Jasm Sison <jasm.sison@gmail.com>
2016-05-11 16:25:01 +0200

    Removed unnecessary CFBridgingRelease...

--- jre_emul/Classes/com/google/j2objc/net/IosHttpURLConnection.java
+++ jre_emul/Classes/com/google/j2objc/net/IosHttpURLConnection.java
@@ -577,7 +577,7 @@ public class IosHttpURLConnection extends HttpURLConnection {
     
     for (CFIndex i=0; i<count; i++) {
       SecCertificateRef certificate = SecTrustGetCertificateAtIndex(serverTrust, i);
-      NSData *remoteCertificateData = CFBridgingRelease(SecCertificateCopyData(certificate));
+      NSData* remoteCertificateData = SecCertificateCopyData(certificate);
       IOSByteArray* rawCert = [IOSByteArray arrayWithBytes:[remoteCertificateData bytes] count:[remoteCertificateData length]];
       [self addToCertificateListWithByteArray:rawCert];
     }
